731fcc5ae46bf9b9a95479e3c52b7773c279e52b
Svetlana Popova <pss96.svetlana@gmail.com>
2023-06-22 10:00:44 +0300

    Change tests 1 and 2; Remove actual_result = None

--- homework.py
+++ homework.py
@@ -10,10 +10,10 @@ def test_dark_theme_by_time():
 
     is_dark_theme = None
 
-    if not time(hour=6) < current_time < time(hour=22):
-        is_dark_theme = True
-    else:
+    if time(hour=6) < current_time < time(hour=22):
         is_dark_theme = False
+    else:
+        is_dark_theme = True
 
     assert is_dark_theme is True
 
@@ -35,10 +35,10 @@ def test_dark_theme_by_time_and_user_choice():
 
     if dark_theme_enabled_by_user is not None:
         is_dark_theme = dark_theme_enabled_by_user
-    elif not time(hour=6) < current_time < time(hour=22):
-        is_dark_theme = True
-    else:
+    elif time(hour=6) < current_time < time(hour=22):
         is_dark_theme = False
+    else:
+        is_dark_theme = True
 
     assert is_dark_theme is True
 
@@ -102,18 +102,15 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
     actual_result = print_name_and_argument_of_function(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
     actual_result = print_name_and_argument_of_function(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
     actual_result = print_name_and_argument_of_function(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
